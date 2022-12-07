5eb747c478f1565a2b56087e5ec90375d9a542a8
abhiraj2912 <abhirajaravind408@gmail.com>
2022-12-07 21:35:58 +0530

    first commit

---
+++ src/main/java/com/nest/shoppingappbackend/ShoppingappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingappBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/shoppingappbackend/ShoppingappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.shoppingappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
